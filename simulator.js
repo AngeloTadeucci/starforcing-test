df712f16fc44ffe6104444c3f63f37efbdf24d12
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2026-05-04 16:45:43 -0300

    feat: add applyRateModifiers (safeguard, star catching)

--- simulator.js
+++ simulator.js
@@ -17,7 +17,38 @@
     return 20;
   }
 
+  function applyRateModifiers(currentStar, opts) {
+    const base = global.KMS_RATES[currentStar];
+    let s = base[0], m = base[1], d = base[2], b = base[3];
+
+    const sgActive =
+      opts.safeguard &&
+      currentStar >= 15 && currentStar <= 17 &&
+      !(opts.event === "fivetenfifteen" && currentStar === 15);
+    if (sgActive) {
+      m += b;
+      b = 0;
+    }
+
+    if (opts.starCatching) {
+      s = Math.min(1, s * 1.05);
+      const left = 1 - s;
+      if (d > 0) {
+        const denom = d + b;
+        d = denom > 0 ? d * left / denom : 0;
+        b = left - d;
+      } else {
+        const denom = m + b;
+        m = denom > 0 ? m * left / denom : left;
+        b = left - m;
+      }
+    }
+
+    return [s, m, d, b];
+  }
+
   global.SF = global.SF || {};
   global.SF.baseCost = baseCost;
   global.SF.boomDropStar = boomDropStar;
+  global.SF.applyRateModifiers = applyRateModifiers;
 })(window);
